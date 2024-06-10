9026aca30730d1ac095e3f72d6999c99f93c5d16
ThisuraPerera09 <thisuraperera09@gmail.com>
2024-06-10 22:36:48 +0530

    change

--- quiz-server/src/main/java/com/dailycodework/quizonline/controller/QuestionController.java
+++ quiz-server/src/main/java/com/dailycodework/quizonline/controller/QuestionController.java
@@ -16,9 +16,7 @@ import java.util.Optional;
 
 import static org.springframework.http.HttpStatus.CREATED;
 
-/**
- * @author Simpson Alfred
- */
+
 
 @CrossOrigin("http://localhost:5173")
 @RestController
